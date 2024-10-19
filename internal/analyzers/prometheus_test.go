24cd78f9f2fa0523300f0bfac44780d2fbfe0fc0
Hélia Barroso <helia_barroso@hotmail.com>
2024-10-19 16:23:37 +0100

    [feat] - prom unit

---
+++ internal/analyzers/prometheus_test.go
@@ -0,0 +1,81 @@
+// Copyright 2024 The prometheus-operator Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package analyzers
+
+import (
+	"context"
+	"testing"
+
+	"github.com/prometheus-operator/poctl/internal/k8sutil"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	monitoringclient "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned/fake"
+	"k8s.io/apimachinery/pkg/runtime"
+	clienttesting "k8s.io/client-go/testing"
+	"k8s.io/client-go/kubernetes/fake"
+	rbacv1 "k8s.io/api/rbac/v1"
+)
+
+func TestPrometheusAnalyzer(t *testing.T) {
+	type testCase struct {
+		name                string
+		namespace           string
+		getMockedClientSets func(tc testCase) k8sutil.ClientSets
+		shouldFail          bool
+	}
+
+	tests := []testCase{
+		{
+			name:      "PrometheusServiceAccountNotFound",
+			namespace: "test",
+			shouldFail: true,
+			getMockedClientSets: func(tc testCase) k8sutil.ClientSets {
+				mClient := monitoringclient.NewSimpleClientset(&monitoringv1.PrometheusList{})
+				mClient.PrependReactor("get", "prometheuses", func(_ clienttesting.Action) (bool, runtime.Object, error) {
+					return true, &monitoringv1.Prometheus{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      tc.name,
+							Namespace: tc.namespace,
+						},
+					}, nil
+				})
+
+				kClient := fake.NewSimpleClientset(&rbacv1.ClusterRoleBindingList{})
+				kClient.PrependReactor("list", "clusterrolebindings", func(_ clienttesting.Action) (bool, runtime.Object, error) {
+					return true, nil, errors.NewInternalError(nil)
+				})
+
+				return k8sutil.ClientSets{
+					MClient: mClient,
+					KClient: kClient,
+				}
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			clientSets := tc.getMockedClientSets(tc)
+			err := RunPrometheusAnalyzer(context.Background(), &clientSets, tc.name, tc.namespace)
+			if tc.shouldFail {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
